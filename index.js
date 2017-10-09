d544e0e706a6283424baa54e750734c49867059d
wj008 <26029682@qq.com>
2017-10-10 01:19:14 +0800

    .

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const Sdopx = require('sdopx').Sdopx;
 const helper = require('think-helper');
 
 module.exports = class {
-	
-  constructor(viewFile, viewData, config) {
-	this.opx=new Sdopx();
-	this.opx.setTemplateDir(config.viewPath);
-    this.viewFile = viewFile;
-    this.viewData = viewData;
-	this.config=config;
-    this.handleOptions = config.options;
-  }
 
-  render() {
-    if (helper.isFunction(this.config.beforeRender)) {
-      this.config.beforeRender(this.opx, this.handleOptions);
+    constructor(viewFile, viewData, config) {
+        if (config.extname) {
+            var ext = config.extname.replace(/^\.?/, '');
+            Sdopx.extension = ext;
+        }
+        this.opx = new Sdopx();
+        this.opx.setTemplateDir(config.viewPath);
+        this.viewFile = viewFile;
+        this.viewData = viewData;
+        this.config = config;
+        this.handleOptions = config.options;
     }
-	this.opx._book=this.viewData;
-	return this.opx.fetch('file:' + this.viewFile);
-  }
+
+    render() {
+        if (helper.isFunction(this.config.beforeRender)) {
+            this.config.beforeRender(this.opx, this.handleOptions);
+        }
+        this.opx._book = this.viewData;
+        return this.opx.fetch('file:' + this.viewFile);
+    }
 };
